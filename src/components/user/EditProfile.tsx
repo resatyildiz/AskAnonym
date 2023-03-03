a9cf839bef361add8165beaf3f8c1939b71cc384
Reşat YILDIZ <resat328@gmail.com>
2023-03-03 10:31:24 +0300

    Create edit profile content structure

--- src/components/user/EditProfile.tsx
+++ src/components/user/EditProfile.tsx
@@ -1,12 +1,101 @@
-import React, { FC } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { useUser } from "@supabase/auth-helpers-react";
+import React, { FC, Fragment, useState } from "react";
 import Button from "../common/button/Button";
-
+import Input from "../common/input/Input";
 interface EditProfileProps {}
 
 const EditProfile: FC<EditProfileProps> = ({}) => {
+  const [open, setOpen] = useState(false);
+  const user = useUser();
+
+  const handleOnClickEditButton = () => setOpen(true);
   return (
     <>
-      <Button variant="outlined">Edit Profile</Button>
+      <Button variant="outlined" onClick={handleOnClickEditButton}>
+        Edit Profile
+      </Button>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto relative w-screen max-w-md">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-in-out duration-500"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="ease-in-out duration-500"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <div className="absolute top-0 left-0 -ml-8 flex pt-4 pr-2 sm:-ml-10 sm:pr-4">
+                        <button
+                          type="button"
+                          className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                          onClick={() => setOpen(false)}
+                        >
+                          <span className="sr-only">Close panel</span>
+                          <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                      </div>
+                    </Transition.Child>
+                    <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                      <div className="px-4 sm:px-6">
+                        <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+                          Edit Profile
+                        </Dialog.Title>
+                      </div>
+                      <hr className="my-3" />
+                      <div className="mt-6 flex flex-col px-4 sm:px-6 gap-y-4">
+                        {/* Your content */}
+                        <Input
+                          id={"userName"}
+                          name={"userName"}
+                          label={"User Name"}
+                          value={user?.user_metadata.username}
+                          disabled
+                        />
+                        <Input
+                          id={"biograpyh"}
+                          name={"biograpyh"}
+                          label={"Biography"}
+                          value={user?.user_metadata.username}
+                          maxLength={144}
+                          disabled
+                        />
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </>
   );
 };
