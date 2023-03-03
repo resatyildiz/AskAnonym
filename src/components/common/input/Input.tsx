3d8f8488d341e03d3795d064f5f8d8686effb3c0
Reşat YILDIZ <resat328@gmail.com>
2023-03-03 10:25:44 +0300

    Fix polymorphic input structure

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,40 +1,24 @@
-import React, { ChangeEventHandler, HTMLInputTypeAttribute } from "react";
-
-export interface InputProps {
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  id: string;
   name: string;
   label: string;
-  value?: string | ReadonlyArray<string> | number | undefined;
-  required: boolean | undefined;
-  type?: HTMLInputTypeAttribute | undefined;
-  placeholder?: string;
-  onChange?: ChangeEventHandler<HTMLInputElement> | undefined;
 }
 
-function Input({
-  label,
-  name,
-  type,
-  required,
-  placeholder,
-  value,
-  onChange,
-}: InputProps) {
+function Input(props: InputProps) {
+  const { label, ...inputProps } = props;
   return (
     <div>
-      <label htmlFor={name} className="block text-sm font-medium text-gray-700">
-        {label}
+      <label
+        htmlFor={inputProps.name}
+        className="block text-sm font-medium text-gray-700"
+      >
+        {props.label}
       </label>
       <div className="mt-1">
         <input
-          id={name}
-          name={name}
-          type={type}
-          autoComplete={name}
-          required={required}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-          className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-purple-500 focus:outline-none focus:ring-purple-500 sm:text-sm"
+          {...inputProps}
+          className={`block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-purple-500 focus:outline-none focus:ring-purple-500 sm:text-sm ${inputProps.className}`}
         />
       </div>
     </div>
